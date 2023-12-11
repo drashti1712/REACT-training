99d267f07e9e13b7b9aabc04bbb0c232454390c7
Banuprakash <banuprakashc@yahoo.co.in>
2023-12-11 15:18:03 +0530

    day 1

--- customerapp/src/components/CustomerList.jsx
+++ customerapp/src/components/CustomerList.jsx
@@ -1,15 +1,24 @@
 import { Component } from 'react';
 import customers from '../data';
 import CustomerRow from './CustomerRow';
+import Filter from './Filter';
 
 export default class CustomerList extends Component {
     state = {
-        customers: customers
+        customers: customers,
+        original: customers
+    }
+
+    filterCustomers(txt) {
+        let custs = this.state.original.filter(c => c.name.toUpperCase().indexOf(txt.toUpperCase()) >= 0);
+        this.setState({
+            customers: custs
+        })
     }
 
     deleteCustomer(id) {
         console.log("<CustomerList />", id)
-        let custs = this.state.customers.filter(c => c.id !== id);
+        let custs = this.state.original.filter(c => c.id !== id);
         // this.state.customers = custs; // won't work --> won't reconcil --> not force render();
         console.log(this.state.customers.length);
 
@@ -21,9 +30,10 @@ export default class CustomerList extends Component {
 
     render() {
         return <div>
+            <Filter filterEvent={(txt) => this.filterCustomers(txt)} />
             {
                 this.state.customers.map(c => <CustomerRow
-                    key ={c.id}
+                    key={c.id}
                     delEvent={(id) => this.deleteCustomer(id)}
                     customer={c} />)
             }
